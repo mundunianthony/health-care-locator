87b82395856c6682ef540d4bff17506f7e0f968d
munduni anthony <mundunianthony@gmail.com>
2025-04-24 10:43:25 +0300

    refactor HeartBtn component to use React Native elements and TypeScript types

--- components/HeartBtn.tsx
+++ components/HeartBtn.tsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
-import { FaHeart } from "react-icons/fa6";
+import { TouchableOpacity, StyleSheet } from "react-native";
+import { Icon } from "react-native-elements";
 import useAuthCheck from "../hooks/useAuthCheck";
 import { useMutation } from "react-query";
 import { useAuth0 } from "@auth0/auth0-react";
@@ -7,8 +8,12 @@ import UserDetailContext from "../context/UserDetailContext";
 import { toFav } from "../utils/api";
 import { checkFavourites, updateFavourites } from "../utils/common";
 
-const HeartBtn = ({ id }) => {
-  const [heartColor, setHeartColor] = useState("white");
+interface HeartBtnProps {
+  id: string;
+}
+
+const HeartBtn: React.FC<HeartBtnProps> = ({ id }) => {
+  const [heartColor, setHeartColor] = useState<string>("white");
   const { validateLogin } = useAuthCheck();
   const { user } = useAuth0();
 
@@ -19,14 +24,14 @@ const HeartBtn = ({ id }) => {
 
   useEffect(() => {
     setHeartColor(() => checkFavourites(id, favourites));
-  }, [favourites]);
+  }, [favourites, id]);
 
   const { mutate } = useMutation({
-    mutationFn: () => toFav(id, user?.email, token),
+    mutationFn: () => toFav(id, user?.email ?? "", token),
     onSuccess: () => {
       setUserDetails((prev) => ({
         ...prev,
-        favourites: updateFavourites(id, prev.favourites)
+        favourites: updateFavourites(id, prev.favourites),
       }));
     },
   });
@@ -39,16 +44,16 @@ const HeartBtn = ({ id }) => {
   };
 
   return (
-    <FaHeart
-      onClick={(e) => {
-        e.stopPropagation();
-        handleLike();
-      }}
-      color={heartColor}
-      size={23}
-      className="cursor-pointer drop-shadow-sm"
-    />
+    <TouchableOpacity onPress={handleLike} style={styles.heart}>
+      <Icon name="heart" type="font-awesome" color={heartColor} size={23} />
+    </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  heart: {
+    padding: 5,
+  },
+});
 
 export default HeartBtn;
