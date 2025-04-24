8cd9261643754206120edc8016d0df979fc15061
munduni anthony <mundunianthony@gmail.com>
2025-04-24 23:42:00 +0300

    add more components

---
+++ constants/data.ts
@@ -0,0 +1,26 @@
+import img1 from '../assets/img1.png'
+import img2 from '../assets/img2.png'
+import img3 from '../assets/img3.png'
+import img4 from '../assets/img4.png'
+import img5 from '../assets/img5.png'
+import blog1 from '../assets/blog1.jpg'
+import blog2 from '../assets/blog2.jpg'
+import blog3 from '../assets/blog3.jpg'
+import blog4 from '../assets/blog4.jpg'
+
+// icons
+import {
+  FaFacebook,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+  FaYoutube,
+  FaTiktok
+} from "react-icons/fa6";
+
+// properties data
+export const PROPERTIES = [
+  // ... (array of property objects)
+];
+
+// ... (other exports)
