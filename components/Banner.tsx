b01f572d42c0d920ccc0e49b206a8fd059f88074
munduni anthony <mundunianthony@gmail.com>
2025-04-24 10:24:16 +0300

    add banner component

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,1 +1,26 @@
-// ...empty file...
+import React from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+import bannerImg from '../assets/banner.png'; // Assuming a banner image
+
+const Banner: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={bannerImg} style={styles.image} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    resizeMode: 'cover',
+  },
+});
+
+export default Banner;
