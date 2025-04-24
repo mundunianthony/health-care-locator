83969c01b8b5e7442b3201f6a34ecb3e44388da5
munduni anthony <mundunianthony@gmail.com>
2025-04-24 10:26:22 +0300

    Blog details component

---
+++ components/BlogDetails.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { View, Text, Image, ScrollView, TouchableOpacity, StyleSheet } from 'react-native';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import { BLOGS } from '../constant/data';
+
+const BlogDetails: React.FC = () => {
+  const route = useRoute<any>();
+  const navigation = useNavigation();
+  const blogId = route.params?.blogId;
+  const blog = BLOGS.find((b) => b.id === parseInt(blogId, 10));
+
+  if (!blog) {
+    return (
+      <View style={styles.errorContainer}>
+        <Text style={styles.errorText}>Blog not found 😔</Text>
+      </View>
+    );
+  }
+
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>{blog.title}</Text>
+      <Image source={{ uri: blog.image }} style={styles.image} />
+      <Text style={styles.content}>{blog.content}</Text>
+      <TouchableOpacity
+        onPress={() => navigation.goBack()}
+        style={styles.backButton}
+      >
+        <Text style={styles.backButtonText}>Back to Blogs</Text>
+      </TouchableOpacity>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    borderRadius: 8,
+    marginBottom: 10,
+  },
+  content: {
+    fontSize: 16,
+    lineHeight: 24,
+    marginBottom: 20,
+  },
+  backButton: {
+    backgroundColor: '#007AFF',
+    padding: 10,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  backButtonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  errorContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  errorText: {
+    fontSize: 18,
+    color: '#666',
+  },
+});
+
+export default BlogDetails;
